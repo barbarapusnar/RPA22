2adb6ebab9b7ffb5a69414be60ccf1a49d43179a
Barbara Pušnar <barbara.pusnar@scng.si>
2022-04-09 16:34:49 +0200

    bbbb

---
+++ VajaCodeFirst/VajaCodeFirst/Models/Izpit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VajaCodeFirst.Models
+{
+    public class Izpit
+    {
+        public int Id { get; set; }
+        
+        public DateTime Datum { get; set; }
+        public int StudentID { get; set; }
+        public int PredmetID { get; set; }
+        public int Ocena { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Predmet Predmet { get; set; }    
+    }
+}
